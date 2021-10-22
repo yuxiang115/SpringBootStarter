113e2e67f94dd1ed36f8379217a2411203f4907e
Xiang Yu <yuxiang115@126.com>
2021-10-22 01:54:53 -0500

    Starter

---
+++ src/main/java/com/xiang/applicationStarter/ApplicationStarterApplication.java
@@ -0,0 +1,13 @@
+package com.xiang.applicationStarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xiang/applicationStarter/ApplicationStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiang.applicationStarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
